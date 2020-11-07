01ea3564559440ab352c23ab142d96c345496d15
swarajlaha <swaraj.srj@gmail.com>
2020-11-07 21:18:35 +0530

    desc table

--- code/dynamo db/sandbox/table-ops.js
+++ code/dynamo db/sandbox/table-ops.js
@@ -6,10 +6,23 @@ const dynamodb = new AWS.DynamoDB();
 /**
  * List tables in console.
  */
-dynamodb.listTables({}, (err, data)=>{
+/* dynamodb.listTables({}, (err, data)=>{
     if(err) {
         console.log(err);
     } else {
         console.log(data);
     }
-});
+}); */
+
+/**
+ * Describe tables in console.
+ */
+dynamodb.describeTable({
+        TableName: "hands_on_notes"
+    }, (err, data)=>{
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(JSON.stringify(data, null, 2));
+        }
+    });
