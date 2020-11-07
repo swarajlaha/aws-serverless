51918939b25ec7108a423ad9be4a66a92502061e
swarajlaha <swaraj.srj@gmail.com>
2020-11-07 22:27:13 +0530

    conditional PUT

---
+++ code/dynamo db/sandbox/conditional.js
@@ -0,0 +1,27 @@
+const AWS = require("aws-sdk");
+AWS.config.update({ region: 'ap-south-1' });
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+docClient.put({
+    TableName: 'td_notes_sdk',
+    Item: {
+        user_id: 'ABC',
+        timestamp: 1,
+        title: 'New Title',
+        content: 'New Content'
+    },
+    ConditionExpression: '#t <> :t',
+    ExpressionAttributeNames: {
+        '#t': 'timestamp'
+    },
+    ExpressionAttributeValues: {
+        ':t': 1
+    }
+}, (err, data)=>{
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
