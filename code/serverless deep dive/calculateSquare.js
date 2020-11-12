d1c9edaf470b7049386ca106ad9ec76b0af4431b
swarajlaha <swaraj.srj@gmail.com>
2020-11-12 20:11:55 +0530

    lambda function to calculate sq

---
+++ code/serverless deep dive/calculateSquare.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'ap-south-1' });
+
+var lambda = new AWS.Lambda();
+
+exports.handler = async (event) => {
+  let number = event.number;
+  let payload = JSON.stringify({
+      operation: "multiply",
+      input: {
+          operand1: number,
+          operand2: number
+      }
+  });
+  
+  let params = {
+      FunctionName: "calculator",
+      InvocationType: "RequestResponse",
+      Payload: payload
+  }
+  
+  let data = await lambda.invoke(params).promise();
+  
+  let result = JSON.parse(data.Payload);
+  
+  return result.body;
+};
