9de1c6b144456e821b102eed4963d1c327aef281
swarajlaha <swaraj.srj@gmail.com>
2020-11-19 11:48:55 +0530

    serverless framework template

---
+++ code/ci cd/sls-cicd/handler.js
@@ -0,0 +1,15 @@
+'use strict';
+const moment = require('moment');
+
+module.exports.logger = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Serverless CI/CD Demo',
+        version: "v1.0",
+        timestamp: moment().unix()
+      }
+    )
+  };
+};
